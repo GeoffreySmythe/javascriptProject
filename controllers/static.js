be99df000b4448292c95d85e41b6556372d3c035
vxm666 <vxm666@cs.bham.ac.uk>
2017-04-02 21:35:08 +0300

    Gulp and minor changes

--- controllers/static.js
+++ controllers/static.js
@@ -1,6 +1,10 @@
+var express = require ('express');
 var router = require ('express').Router ()
+
 router.get ('/', function (req, res) {
     res.sendfile ('layouts/posts.html')
 })
+                // points to the current file's directory (__dirname)
+router.use (express.static (__dirname + '/../assets'))
 
 module.exports = router
